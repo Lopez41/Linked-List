c489285f91ff3e0922c53c3d0a99e8b64ddb5fb8
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-06-28 20:00:52 -0500

    added constructor,deconstructor and public methods

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,35 @@
-/*
- * your comment header here
- */
+/*******************
+*coding assignment 06: Linked List
+*Name: Adrian Lopez
+*Date: 06/28/23
+*Status:in progress
+*Language: C++
+*File: linkedlist.h
+*******************/
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "data.h"
+
+class LinkedList
+{
+public:
+    //constructor
+    LinkedList();
+    //deconstructor
+    ~LinkedList();
+     
+    //methods
+    bool addNode(int,string*);
+    bool deleteNode(int);
+    bool getNode(int,Data*);
+    void printList(bool = false);
+    int getCount();
+    void clearList();
+    bool exsit(int);
+
+
+    
+    
+  
+}
