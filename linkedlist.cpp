c8a1801f7978d6919683c376cf67003c53ad5794
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-06-29 22:47:11 -0500

    added constructor and deconstructor and bool LinkedList::addNode(int id, string* data) which adds new node to the linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,49 @@
-/*
- * your comment header here
- */
+/*******************
+*coding assignment 06: Linked List
+*Name: Adrian Lopez
+*Date: 06/19/23
+*Status:in progress
+*Language: C++
+*File: stack.cpp
+*******************/
+#include "linkedlist.h"
+
+LinkedList::LinkedList() {
+    head = nullptr;
+    count = 0;
+}
+
+LinkedList::~LinkedList() {
+    clearList();
+}
+
+bool LinkedList::addNode(int id, string* data) {
+  bool success = false; // Variable to track the success of adding a node
+
+    if (!exists(id)) { // Check if a node with the same ID already exists
+        Node* newNode = createNode(id, data);
+        if (newNode != nullptr) { // Check if memory allocation for the new node is successful
+            if (head == nullptr) {
+                head = newNode;
+            } else {
+                Node* current = head;
+                while (current->next != nullptr) {
+                    current = current->next;
+                }
+                current->next = newNode;
+                newNode->prev = current;
+            }
+
+            count++;
+            success = true; // Node added successfully
+        }
+    }
+
+    return success;
+}
+
+
+
+
+
+
